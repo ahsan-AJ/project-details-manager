0bec9c6ee0b8edc28105fda2c5d2402b90b3c393
Ahsan <ahsan-jamal@hotmail.com>
2019-01-04 19:14:27 +0500

    Initial Commit

---
+++ src/components/common/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div>
+            Oops! Not found
+        </div>
+    )
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Steps for Creating Redux App;
+// 1 import {createStore} from Redux;
+// Import provider component so that application has access to the store
+// 2 import {provider} from 'react-redux';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+// 2 Create a store variable and call createStore();
+// 3 Combine different reducers into a single reducer and pass it to the create store function
+
+const store = createStore(rootReducer);
+
+// 4 Wrap the App component in the Provider component with store as a prop
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
